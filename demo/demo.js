3491c0e394fc6ede68398c05f91d5797c770fc25
İsmail Atkurt <ismail.atkurt@gmail.com>
2016-03-28 22:42:31 +0300

    model binding added.

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,19 @@
-var app = angular.module('asideMenuDemo', ['asideModule']);
 
 
 var app = angular.module('angularSmiliesRatingDemo', ['angular-smilies-rating']);
+
+app
+  .controller('DemoController', DemoController);
+
+DemoController.$inject = ['$scope'];
+
+function DemoController($scope) {
+
+
+  var self = this;
+
+  $scope.values = "Angular Smilies Rating Demo";
+
+  $scope.demoModel = "";
+
+}
--- src/angular-smilies-rating.js
+++ src/angular-smilies-rating.js
@@ -2,9 +2,31 @@
 
   var smiliesRating = angular.module('angular-smilies-rating', []);
 
+  var smileyTemplate = '<input id="$1" type="radio" name="$2" ng-model="$3" value="$1" /><label class="drinkcard-cc $1" for="$1"></label>';
+
   smiliesRating.directive('smiliesRating', function () {
     return {
-      template: '<div class="cc-selector-2"><input id="sad" type="radio" name="mood" ng-model="_help.mood" value="sad" /> <label class="drinkcard-cc sad" for="sad"></label><input id="neutral" type="radio" name="mood" ng-model="_help.mood" value="neutral" /> <label class="drinkcard-cc neutral" for="neutral"></label><input id="normal" type="radio" name="mood" ng-model="_help.mood" value="normal" /> <label class="drinkcard-cc normal" for="normal"></label><input id="happy" type="radio" name="mood" ng-model="_help.mood" value="happy" /> <label class="drinkcard-cc happy" for="happy"></label></div>'
+      restrict: 'E',
+      require: 'ngModel',
+      transclude: true,
+      scope: {
+        values: '@',
+        ngModel: '=',
+        width: '@'
+      },
+      template: '<div class="cc-selector-2"><input id="sad" type="radio" name="mood" ng-model="mood" value="sad" /> <label class="drinkcard-cc sad" for="sad"></label><input id="neutral" type="radio" name="mood" ng-model="mood" value="neutral" /> <label class="drinkcard-cc neutral" for="neutral"></label><input id="normal" type="radio" name="mood" ng-model="mood" value="normal" /> <label class="drinkcard-cc normal" for="normal"></label><input id="happy" type="radio" name="mood" ng-model="mood" value="happy" /> <label class="drinkcard-cc happy" for="happy"></label></div>',
+      link: function (scope, element, attrs) {
+        console.log(scope.values);
+        scope.ngModel = 'hopp';
+        scope.mood = '';
+
+        element.bind('click', function() {
+          scope.$apply(function() {
+            scope.ngModel = scope.mood;
+          });
+        });
+
+      }
     }
   });
 
